75636d19bf69390f3692e719888fa4e4656c7614
buronk <nukonabehabimi@gmail.com>
2019-07-31 01:14:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,8 +10,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
-channel_bot_test = [channel for channel in client.get_all_channels() if channel.name == 'test'][0]
-await client.send_message(channel_bot_test, '勝手に喋るよ')
 
 @bot.command()
 async def ping(ctx):
