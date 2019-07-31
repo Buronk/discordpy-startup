f78e948506d79b536fc9e0731cf6728d8741a535
buronk <nukonabehabimi@gmail.com>
2019-07-31 21:49:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,15 +6,17 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+import discord
 
+client = discord.Client()
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
+@client.event
+async def on_voice_state_update(member, before, after): 
+    if member.guild.id == 605720617924165646 and (before.channel != after.channel):
+        alert_channel = client.get_channel(605720617924165646)
+        if before.channel is None: 
+            msg = {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
 
 
 bot.run(token)
