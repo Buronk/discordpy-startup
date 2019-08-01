02ac828ec35ee644916601431be4875145b81c51
buronk <nukonabehabimi@gmail.com>
2019-08-01 19:05:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,8 +10,8 @@ client = discord.Client()
 
 @client.event
 async def on_voice_state_update(member, before, after): 
-    if member.guild.id == 605720617924165646 and (before.channel != after.channel):
-        alert_channel = client.get_channel(605720617924165644)
+    if member.guild.id == 605720617924165644 and (before.channel != after.channel):
+        alert_channel = client.get_channel(605720617924165646)
     if before.channel is None: 
         await alert_channel.send(f'{member.name} が {after.channel.name} に参加しました。')
 
